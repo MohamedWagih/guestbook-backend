bad1cf435dd266877401f3b5c56bda22f0182d06
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-07-01 18:32:06 +0200

    :construction: init db

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const dbConfig = require("./app/config/db.config");
+const db = require("./app/models");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Guestbook app!" });
