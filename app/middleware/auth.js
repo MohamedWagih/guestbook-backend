b38de5fb2777bb0eb0daef0a42e77b923dbbf6a4
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-07-03 23:21:49 +0200

    :bug: fix typo

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -4,7 +4,7 @@ const db = require("../models");
 const User = db.user;
 
 const verifyToken = (req, res, next) => {
-  let token = req.headers["x-access-token"];
+  let token = req.headers["x-auth-token"];
 
   if (!token) {
     return res.status(403).send({ message: "No token provided!" });
